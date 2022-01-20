c4dc723b01a9f4cb473c5dc901e9082b21bec77e
PTSkAlman <joel.abrahamsson@elev.ga.ntig.se>
2022-01-20 13:39:38 +0100

    Encrypt method

--- src/Kryptering.java
+++ src/Kryptering.java
@@ -1,2 +1,21 @@
 public class Kryptering {
+    public static void main(String[] args) {
+        char key = 'L';
+        String encryptedMessage = encrypt("hej", key);
+        System.out.println(encrypt("hej", key));
+        decrypt(encryptedMessage, key);
+    }
+
+    private static String decrypt(String encryptedMessage, char key) {
+
+        return "";
+    }
+
+    private static String encrypt(String message, char key) {
+        String encryptedMessage = "";
+        for (int i = 0; i < message.length(); i++) {
+            encryptedMessage += Integer.toBinaryString(message.charAt(i)^key) + " ";
+        }
+        return encryptedMessage;
+    }
 }
