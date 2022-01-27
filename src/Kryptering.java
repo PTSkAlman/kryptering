ad1a7c882cea8f77bb43a7e5f871297732e99350
PTSkAlman <joel.abrahamsson@elev.ga.ntig.se>
2022-01-27 12:52:26 +0100

    decrypt + byte[]

--- src/Kryptering.java
+++ src/Kryptering.java
@@ -1,21 +1,25 @@
 public class Kryptering {
     public static void main(String[] args) {
         char key = 'L';
-        String encryptedMessage = encrypt("hej", key);
+        byte[] encryptedMessage = encrypt("hej", key);
         System.out.println(encrypt("hej", key));
-        decrypt(encryptedMessage, key);
+        System.out.println( decrypt(encryptedMessage, key));
+
     }
 
-    private static String decrypt(String encryptedMessage, char key) {
-
-        return "";
+    private static String decrypt(byte[] encryptedMessage, char key) {
+        byte[] decrypt = new byte[encryptedMessage.length];
+        for (int i = 0; i < encryptedMessage.length; i++) {
+            decrypt[i] = (byte) (encryptedMessage[i]^key);
+        }
+        return new String(decrypt);
     }
 
-    private static String encrypt(String message, char key) {
+    private static byte[] encrypt(String message, char key) {
         String encryptedMessage = "";
         for (int i = 0; i < message.length(); i++) {
             encryptedMessage += Integer.toBinaryString(message.charAt(i)^key) + " ";
         }
-        return encryptedMessage;
+        return encryptedMessage.getBytes();
     }
 }
